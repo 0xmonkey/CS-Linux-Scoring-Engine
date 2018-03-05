3d4ac6ae53f575f4806cc34df79bab8b3e78d1c2
Tyler Michels <tjmichels4455@gmail.com>
2018-03-04 22:00:33 -0600

    added all variables to currentSettings object

--- EECS582/ScoringEngine/src/currentSettings.java
+++ EECS582/ScoringEngine/src/currentSettings.java
@@ -7,5 +7,38 @@ public class currentSettings {
 	String anon_login_setting;
 	
 	int ssh_v_2_sc;
-	int ssh_v_2_setting;
+	String ssh_v_2_setting;
+	
+	int ftp_service_sc;
+	String ftp_service_setting;
+	
+	int sql_service_sc;
+	String sql_service_setting;
+	
+	int www_service_sc;
+	String www_service_setting;
+	
+	int dns_service_sc;
+	String dns_service_setting;
+	
+	int netcat_backdoor_sc;
+	String netcat_backdoor_setting;
+	
+	int sudo_pw_sc;
+	String sudo_pw_setting;
+	
+	int rm_sudo_usr_sc;
+	String rm_sudo_usr_setting;
+	
+	int rm_usr_sc;
+	String rm_usr_setting;
+	
+	int all_usr_pass_sc;
+	String all_usr_pass_setting;
+	
+	int no_files_sc;
+	String no_files_setting;
+	
+	int mal_software_sc;
+	String mal_software_setting;
 }
